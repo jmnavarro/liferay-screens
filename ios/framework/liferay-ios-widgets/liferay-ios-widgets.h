f87bb1c67865f7a96869f32eeaa9d6fc60a8a36a
JM <jose.navarro@liferay.com>
2014-07-03 14:26:13 +0200

    MOBILEWIDG-5 Added bridge header in order to use ObjC code from the swift based widgets

---
+++ ios/framework/liferay-ios-widgets/liferay-ios-widgets.h
@@ -0,0 +1,28 @@
+//
+//  liferay-mobile-widgets.h
+//  liferay-mobile-widgets
+//
+//  Created by jmWork on 03/07/14.
+//  Copyright (c) 2014 Liferay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for liferay-mobile-widgets.
+FOUNDATION_EXPORT double liferay_mobile_widgetsVersionNumber;
+
+//! Project version string for liferay-mobile-widgets.
+FOUNDATION_EXPORT const unsigned char liferay_mobile_widgetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <liferay_mobile_widgets/PublicHeader.h>
+
+#import "MBProgressHUD.h"
+
+// Liferay services
+#import "LRUserService_v62.h"
+
+#import "LRCallback.h"
+#import "LRSession.h"
+
+
+
